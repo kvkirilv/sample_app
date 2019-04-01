9bdfdfeb7ffe8a6798ad5b229f7fc16686c71da4
Kirilov.K.V <kvkirilv@ya.ru>
2019-04-01 13:17:36 +0100

    Finish log in/log out:

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-	resources :users
+  	resources :users
   	root 'static_pages#home'
   	get  '/help',    to: 'static_pages#help'
   	get  '/about',   to: 'static_pages#about'
   	get  '/contact', to: 'static_pages#contact'
   	get  '/signup',  to: 'users#new'
+  	get    'login'   => 'sessions#new'
+  	post   'login'   => 'sessions#create'
+  	delete 'logout'  => 'sessions#destroy'
 end
