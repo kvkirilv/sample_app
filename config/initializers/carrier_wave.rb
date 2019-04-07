7fc46d02310786cf66481381b72e79557aad8e4a
Kirilov.K.V <kvkirilv@ya.ru>
2019-04-07 10:51:33 +0100

    Add permission

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
+    #config.fog_provider = 'fog/aws'
 
     config.fog_credentials = {
 
       :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAWG3N35XEC5WKF6M4',
-      :aws_secret_access_key => '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',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       :region                => 'us-east-2'
     }
     config.fog_directory     =  ENV['S3_BUCKET']
